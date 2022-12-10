deafc820172c5ecb61f363c5b9766ba49e4f4907
Robotix703 <stephane.chatelle@orange.fr>
2022-12-10 21:18:03 +0100

    Fix password hash

--- src/initialization.ts
+++ src/initialization.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 import { baseList } from "./compute/base/list";
 import { baseUser } from "./compute/base/user";
+import bcrypt from "bcrypt";
 
 export async function createUserIfNeeded(): Promise<null>{
     const userNumber = await baseUser.count();
@@ -11,8 +12,11 @@ export async function createUserIfNeeded(): Promise<null>{
         await baseUser.deleteAll();
     }
 
-    await baseUser.register(process.env.USERNAME1, process.env.PASSWORD1, 0);
-    await baseUser.register(process.env.USERNAME2, process.env.PASSWORD2, 1);
+    const password1 = await bcrypt.hash(process.env.PASSWORD1, 10);
+    const password2 = await bcrypt.hash(process.env.PASSWORD2, 10);
+
+    await baseUser.register(process.env.USERNAME1, password1, 0);
+    await baseUser.register(process.env.USERNAME2, password2, 1);
     return;
 }
 
